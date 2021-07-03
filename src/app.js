012fc0685927dcd2ced473032fc1325fe956ec21
thiagodesantos <thiagodesantos23@gmail.com>
2021-07-03 10:32:39 -0300

    remoção dos dados de mock do app.js

--- src/app.js
+++ src/app.js
@@ -13,14 +13,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'conta de agua', amount: 4500}));
-store.dispatch(addExpense({ description: 'conta de gas', createdAt: 1000}));
-store.dispatch(addExpense({ description: 'Aluguel', amount: 109500}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
